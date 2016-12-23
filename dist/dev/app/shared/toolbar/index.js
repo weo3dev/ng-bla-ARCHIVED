501e3cae5c8c9a1bc09a501c17fa907f2833d31a
William ONeal <metavurt@gmail.com>
2016-12-23 13:43:30 -0800

    Removed all files *now* listed in .gitignore. Derp.

--- dist/dev/app/shared/toolbar/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./toolbar.component"));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9zaGFyZWQvdG9vbGJhci9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBR0EseUNBQW9DIiwiZmlsZSI6ImFwcC9zaGFyZWQvdG9vbGJhci9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogVGhpcyBiYXJyZWwgZmlsZSBwcm92aWRlcyB0aGUgZXhwb3J0IGZvciB0aGUgc2hhcmVkIFRvb2xiYXJDb21wb25lbnQuXG4gKi9cbmV4cG9ydCAqIGZyb20gJy4vdG9vbGJhci5jb21wb25lbnQnO1xuIl19
--- dist/dev/app/teams/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./teams.component"));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC90ZWFtcy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBR0EsdUNBQWtDIiwiZmlsZSI6ImFwcC90ZWFtcy9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogVGhpcyBiYXJyZWwgZmlsZSBwcm92aWRlcyB0aGUgZXhwb3J0IGZvciB0aGUgbGF6eSBsb2FkZWQgVGVhbXNDb21wb25lbnQuXG4gKi9cbmV4cG9ydCAqIGZyb20gJy4vdGVhbXMuY29tcG9uZW50JztcbiJdfQ==
