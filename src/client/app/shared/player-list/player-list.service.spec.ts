3894d3379cc32aa20748a2ab6928294919074daa
William ONeal <metavurt@gmail.com>
2017-01-03 19:38:02 -0800

    Moving through tslint errors

--- src/client/app/shared/player-list/player-list.service.spec.ts
+++ src/client/app/shared/player-list/player-list.service.spec.ts
@@ -4,18 +4,18 @@ import { MockBackend } from '@angular/http/testing';
 
 import { Observable } from 'rxjs/Observable';
 
-import { TeamListService } from './team-list.service';
+import { PlayerListService } from './player-list.service';
 
 export function main() {
-  describe('TeamList Service', () => {
-    let teamListService: TeamListService;
+  describe('PlayerList Service', () => {
+    let playerListService: PlayerListService;
     let mockBackend: MockBackend;
 
     beforeEach(() => {
 
       TestBed.configureTestingModule({
         providers: [
-          TeamListService,
+          PlayerListService,
           MockBackend,
           BaseRequestOptions,
           {
@@ -27,19 +27,19 @@ export function main() {
       });
     });
 
-    it('should return an Observable when "getTeams" is called', async(() => {
-      expect(TestBed.get(TeamListService).getTeams()).toEqual(jasmine.any(Observable));
+    it('should return an Observable when "getPlayers" is called', async(() => {
+      expect(TestBed.get(PlayerListService).getPlayers()).toEqual(jasmine.any(Observable));
     }));
 
     it('should resolve to list of teams when "getTeams" is called', async(() => {
-      let teamListService = TestBed.get(TeamListService);
+      let playerListService = TestBed.get(PlayerListService);
       let mockBackend = TestBed.get(MockBackend);
 
       mockBackend.connections.subscribe((c: any) => {
         c.mockRespond(new Response(new ResponseOptions({ body: '["Dijkstra"]' })));
       });
 
-      teamListService.getTeams().subscribe((data: any) => {
+      playerListService.getPlayers().subscribe((data: any) => {
         //expect(data).toEqual(['Dijkstra']);
       });
     }));
